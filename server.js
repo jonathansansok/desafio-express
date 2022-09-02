ea7c7d49c8715a41f3246ba0487c03dd48267b6b
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-09-02 15:21:21 -0300

    ¨js¨

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 const express = require('express')
 const app = express();
 const Contenedor = require('./Contenedor.js');
+
 const server = app.listen(8080, () => console.log("server up!!"));
+
 server.on("error", (error) => console.log(`Error con el servidor`));
 
 let numeroAleatorio = 0;
